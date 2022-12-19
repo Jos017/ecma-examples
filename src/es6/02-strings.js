e117dff2e46e11dfcec336f4eb92a4a9834761a2
Jose Rios <jrbernabe@gmail.com>
2022-12-19 16:26:25 -0400

    Add template literal examples for strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,19 @@
+let hello = 'Hello';
+let world = 'World';
+
+// Traditional Concatenate
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+// Traditional Multi-line strings
+let lorem = 'Esto es un string \n ' + 'esto es otra linea';
+console.log(lorem);
+
+//Multi-line strings Template literals
+let lorem2 = `Esto es un string
+ esto es otra línea`;
+console.log(lorem2);
