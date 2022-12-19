c4cbc830fd6006fe8ebe9f569cb3d8eea8deec48
Jose Rios <jrbernabe@gmail.com>
2022-12-19 16:03:36 -0400

    Add arrow functions example

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,12 @@
+// Traditional function
+function square(num) {
+  return num * num;
+}
+
+// Arrow function
+const square = (num) => {
+  return num * num;
+};
+
+// Arrow function one line
+const square = num => num * num;
