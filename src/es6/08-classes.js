3a5e01e4b2ad1924270c8024f0e0a2ff93acaccb
Jose Rios <jrbernabe@gmail.com>
2022-12-21 12:06:34 -0400

    Add class example

---
+++ src/es6/08-classes.js
@@ -0,0 +1,32 @@
+// class declaration
+class MyClass {}
+// creating an instance
+const newInstance = new MyClass();
+
+class User {
+  // constructor
+  constructor(name, age) {
+    console.log('Nuevo Usuario');
+    this.name = name;
+    this.age = age;
+  }
+  // methods
+  speak() {
+    return 'Hello';
+  }
+  greeting() {
+    return `${this.speak()} ${this.name}`;
+  }
+
+  get uAge() {
+    return this.age
+  }
+  set uAge(n) {
+    this.age = n;
+  }
+}
+
+const jbr = new User('Jose', 28);
+console.log(jbr.greeting());
+console.log(jbr.uAge);
+console.log(jbr.uAge = 20);
