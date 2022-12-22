b2267c342119418ab759157c0d4901ace7e6efc8
Jose Rios <jrbernabe@gmail.com>
2022-12-22 09:54:03 -0400

    Add padding example

---
+++ src/es8/01-string-padding.js
@@ -0,0 +1,7 @@
+const string = 'Hello';
+
+// Returns a string filled at start with _* defined untill length is 7
+console.log(string.padStart(7, '_*'));
+
+// Returns a string filled at end with _* defined untill length is 7
+console.log(string.padEnd(7, '_*'));
