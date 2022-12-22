6136683b9bb574794838e13a0379a4763b55aecb
Jose Rios <jrbernabe@gmail.com>
2022-12-22 09:49:41 -0400

    Add ES8 object entries, keys and values example

---
+++ src/es8/00-entries-keys-values.js
@@ -0,0 +1,15 @@
+const countries = {
+  MX: 'Mexico',
+  CO: 'Colombia',
+  CL: 'Chile',
+  PE: 'Peru',
+};
+
+// Obtain entries
+console.log(Object.entries(countries));
+
+// Obtain keys
+console.log(Object.keys(countries));
+
+// Obtain values
+console.log(Object.values(countries));
